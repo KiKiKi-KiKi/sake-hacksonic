e5a41ea21e954965e872db93681c6c74298311f8
KiKiKi <chaika.design@gmail.com>
2024-08-11 01:15:31 +0900

    :moon_cake: vanilla-extract css
    cf. https://vanilla-extract.style/

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+
 const nextConfig = {};
 
-export default nextConfig;
+const withVanillaExtract = createVanillaExtractPlugin();
+
+export default withVanillaExtract(nextConfig);
