2d765de5188b9db8ce9e68b06d618053078c8cb6
KiKiKi <chaika.design@gmail.com>
2024-08-11 11:04:03 +0900

    :postal_horn: SVG component settings
    cf. https://www.npmjs.com/package/@svgr/webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('next').NextConfig} */
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 
-const nextConfig = {};
-
 const withVanillaExtract = createVanillaExtractPlugin();
+
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push(
+      ...[
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: '@svgr/webpack',
+            },
+          ],
+        },
+      ].filter(Boolean),
+    );
+
+    return config;
+  },
+};
 
 export default withVanillaExtract(nextConfig);
--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const APP_NAME = 'がんばれ肝臓くん！' as const;
+export const APP_NAME = 'Go!Go!KanZo-kun!' as const;
