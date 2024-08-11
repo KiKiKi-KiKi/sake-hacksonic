a9c754a14fefe2336cb72077106288cb615478a9
KiKiKi <chaika.design@gmail.com>
2024-08-11 20:21:42 +0900

    :recycle: Modify design of the Kanzo count down

--- src/features/KanzoCountdown/KanzoCountdown.tsx
+++ src/features/KanzoCountdown/KanzoCountdown.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { FC } from 'react';
 
-import { Box, BoxProps } from '@chakra-ui/react';
+import { Box, BoxProps, Text } from '@chakra-ui/react';
 
 import { Logo } from '@/components/Logo';
 import { useDrank } from '@/hooks/useDrank';
@@ -15,6 +15,7 @@ import { kanzo } from './kanzo.css';
 export const KanzoCountdown: FC<BoxProps> = ({ ...boxProps }) => {
   const { expiredAt } = useDrank();
   const dangerLevel = getDangerLevelByExpiredAt(expiredAt ?? 0);
+  console.log({ dangerLevel });
 
   return (
     <Box
@@ -27,7 +28,18 @@ export const KanzoCountdown: FC<BoxProps> = ({ ...boxProps }) => {
       <Box className={classnames(kanzo, dangerLevel > 3 && 'spin')}>
         <Logo isGood={dangerLevel < 3} fontSize='100px' />
       </Box>
-      <Countdown />
+      {dangerLevel > 0 ? (
+        <Countdown />
+      ) : (
+        <Text
+          color='cyan.400'
+          fontWeight='bold'
+          fontStyle='italic'
+          letterSpacing='0.2em'
+        >
+          KENKOU!
+        </Text>
+      )}
     </Box>
   );
 };
