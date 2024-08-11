bf071581d120302b8d36d54347d2aae15a8b5b07
KiKiKi <chaika.design@gmail.com>
2024-08-11 19:38:34 +0900

    :recycle::tractor::dash:

--- src/app/nomuzo/page.tsx
+++ src/app/nomuzo/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 
 import { PageContainer } from '@/components/layouts/PageContainer';
 import { APP_NAME } from '@/config';
-import { Nomuzo } from '@/features/Nomuzo';
+import { Nomuzo } from '@/features/pages/NomuzoPage';
 
 export const generateMetadata = async (): Promise<Metadata> => {
   return {
