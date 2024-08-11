c59022fbc8014874ba7b903ccc89e9989631d564
KiKiKi <chaika.design@gmail.com>
2024-08-12 00:40:30 +0900

    :beers::beers::beers:

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,17 +1,13 @@
-import Image from 'next/image';
 import { FC } from 'react';
+
+import { RandomSakeIcon } from '../RandomSakeIcon';
 
 import { loader } from './loader.css';
 
 export const Loader: FC = () => {
   return (
     <div className={loader}>
-      <Image
-        src='/icons/beer500.png'
-        width='128'
-        height='128'
-        alt='loading...'
-      />
+      <RandomSakeIcon width={128} height={128} alt='Loading...' />
     </div>
   );
 };
