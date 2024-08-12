47eab321c044c5172c8f07f831ce9dec91effd9f
KiKiKi <chaika.design@gmail.com>
2024-08-12 16:20:42 +0900

    Implement drank drop

--- src/features/pages/NomuzoPage/index.tsx
+++ src/features/pages/NomuzoPage/index.tsx
@@ -6,15 +6,20 @@ import { DrinkCounter } from './DrinkCounter';
 
 export const Nomuzo: FC = () => {
   return (
-    <Box
-      display='flex'
-      alignItems='center'
-      justifyContent='center'
-      width='100%'
-      height='100%'
-      pb='45px'
-    >
-      <DrinkCounter />
+    <Box width='100%' height='100%'>
+      <Box
+        display='flex'
+        alignItems='center'
+        justifyContent='center'
+        width='100%'
+        height='100%'
+        pb='45px'
+      >
+        <DrinkCounter />
+      </Box>
+      {/* <BackgroundContainer>
+        <DrunkerMeter />
+      </BackgroundContainer> */}
     </Box>
   );
 };
